0f486860c75004136b38cd9b8a72e1a40f94fbe4
CosmeNicolas <nicomas7590@gmail.com>
2023-12-14 17:35:02 -0300

    instalamos la libreria de react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      
+      <h1>Hola mundo</h1>
     </>
   )
 }
