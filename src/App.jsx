19e94fa4e755e2adfe0f16d6d483b83844f77c89
CosmeNicolas <nicomas7590@gmail.com>
2023-12-20 20:25:53 -0300

    actulice el archivo app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import FormularioTareas from './components/FormularioTareas';
-import Footer from './components/Footer';
-import {Container} from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import FormularioTareas from "./components/FormularioTareas";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 function App() {
-  
-
   return (
     <>
-    <Container>
-      <h1 className='text-center display-3'>Lista de Tareas</h1>
-     <FormularioTareas></FormularioTareas>
-    </Container>
-    <Footer/>
+      <Container className="my-4">
+        <h1 className="text-center display-3">Lista de tareas</h1>
+        <FormularioTareas></FormularioTareas>
+      </Container>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
