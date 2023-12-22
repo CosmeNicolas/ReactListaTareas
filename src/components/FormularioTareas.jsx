5a8d036396d42a7e7b9ad0f576392c7f292db86c
CosmeNicolas <nicomas7590@gmail.com>
2023-12-22 17:16:54 -0300

    agregamos el evento onChange en el input de la lista de tareas

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,9 +1,13 @@
 import {Button , Form} from 'react-bootstrap';
 // import Form from "react-bootstrap/Form";
 // import Button from "react-bootstrap/Button";
+import { useState } from 'react';
 import ListaTareas from './ListaTareas';
 
 const FormularioTareas = () => {
+  //inicializar el State
+  const [tarea, setTarea] = useState('');
+
   return (
     <section>
       <Form>
@@ -14,6 +18,7 @@ const FormularioTareas = () => {
             placeholder="Ej: tarea 1"
             minLength={3}
             maxLength={50}
+            onChange={(e)=> setTarea(e.target.value)}
           />
           <Button variant="info" className="mx-2" type="submit">Enviar</Button>
         </Form.Group>
